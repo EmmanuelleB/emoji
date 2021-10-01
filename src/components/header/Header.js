3fe875c73777405a09ab1448f44576dbfc51835c
Emmanuelle Baron <emmanuellebaron1@gmail.com>
2021-10-01 14:31:35 +0200

    add margin to container

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,26 @@
 import "./Header.css";
 
 const Header = (props) => {
-  const{title, emoji, seach, setSearch}=props;
+  const { title, emoji, seach, setSearch } = props;
 
-  const handleSearch=(event)=>{
-    let newResult=[];
-    for(let i=0; i< emoji.length;i++){
-      if(emoji[i].keywords.indexOf(event.target.value.toLowerCase()) !== -1){
-        newResult.push(emoji[i])
+  const handleSearch = (event) => {
+    let newResult = [];
+    for (let i = 0; i < emoji.length; i++) {
+      if (emoji[i].keywords.indexOf(event.target.value.toLowerCase()) !== -1) {
+        newResult.push(emoji[i]);
       }
-   
     }
     return setSearch(newResult);
- 
-  }
-
+  };
 
   return (
     <div className="header-container">
       <h1>😎 {title} 😎</h1>
       <input
         className="input-search"
-        placeholder="What emoji are you looking for?"
+        placeholder="What emoji are you looking for ?"
         onChange={handleSearch}
         value={seach}
-        
       />
     </div>
   );
